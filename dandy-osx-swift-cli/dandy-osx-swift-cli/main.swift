ef4638984b831c06876720bcd5c66b02bd807ecf
Jack Palevich <jackpal@google.com>
2014-11-28 09:44:25 -0800

    Initial checkin

---
+++ dandy-osx-swift-cli/dandy-osx-swift-cli/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  dandy-osx-swift-cli
+//
+//  Created by Jack Palevich on 11/28/14.
+//  Copyright (c) 2014 JacksHacks. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
